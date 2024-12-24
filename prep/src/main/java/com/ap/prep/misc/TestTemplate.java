8ff76ac0d86210249d4c9075e9374af8bcdba982
Atul Pundir <atulpundir@Atuls-MacBook-Pro.local>
2024-12-24 12:57:58 +0530

    Initial commit

---
+++ prep/src/main/java/com/ap/prep/misc/TestTemplate.java
@@ -0,0 +1,16 @@
+package com.ap.prep.misc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class TestTemplate {
+
+@Autowired
+private JdbcTemplate jdbcTemplate;
+
+public void addUser(String name) {
+    String sql = "INSERT INTO users (name) VALUES (?)";
+    jdbcTemplate.update(sql, name);
+}
+
+}
